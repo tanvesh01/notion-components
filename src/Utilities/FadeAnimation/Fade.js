4a17c5c94997c3dde4efe32d0e28aaec5a7e527f
tanvesh01 <tanvesh01@DESKTOP-N2FFNK3.localdomain>
2021-01-06 10:19:30 +0530

    its working

--- src/Utilities/FadeAnimation/Fade.js
+++ src/Utilities/FadeAnimation/Fade.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, Fragment } from 'react'
 import './styles.css'
 const Fade = ({ show, children }) => {
   const [render, setRender] = useState(show)
-
+  const mountedStyle = { animation: 'inAnimation 500ms ease-in' }
+  const unmountedStyle = { animation: 'outAnimation 510ms ease-in' }
   useEffect(() => {
     if (show) setRender(true) // if show true the render true
   }, [show])
@@ -13,15 +14,46 @@ const Fade = ({ show, children }) => {
   console.log(render)
   return (
     render && (
-      <div
-        style={{ animation: `${show ? 'fadeIn' : 'fadeOut'} 1s` }}
-        // onAnimationEnd={onAnimationEnd}
-        // onAnimationEndCapture={onAnimationEnd}
-        onAnimationEnd={() => console.log('heello')}
-        key='123'
-      >
-        {children}
-      </div>
+      <Fragment>
+        <div
+          style={show ? mountedStyle : unmountedStyle}
+          // onAnimationEnd={onAnimationEnd}
+          // onAnimationEndCapture={onAnimationEnd}
+          // onAnimationStart={() => console.log('start')}
+          onAnimationEnd={onAnimationEnd}
+          key='123'
+        >
+          {children}
+        </div>
+        <style>
+          {`
+
+           @keyframes inAnimation {
+    0% {
+      transform: scale(0.1);
+      opacity: 0;
+    }
+    60% {
+      transform: scale(1.2);
+      opacity: 1;
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+
+  @keyframes outAnimation {
+    20% {
+      transform: scale(1.2);
+    }
+    100% {
+      transform: scale(0);
+      opacity: 0;
+    }
+  }
+          `}
+        </style>
+      </Fragment>
     )
   )
 }
